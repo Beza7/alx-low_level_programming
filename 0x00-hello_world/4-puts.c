021a0bedcc4c1ffa97467ddff28cf0a1be58a12f
Beza7 <117745289+Beza7@users.noreply.github.com>
2022-12-10 14:42:12 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
 /**
  * main - Entry point
  * Return: Always 0 (success)
- */ 
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multliingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
