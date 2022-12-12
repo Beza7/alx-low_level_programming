57a15f8282200bf7408e63c9b77978a1a7b78221
Beza7 <117745289+Beza7@users.noreply.github.com>
2022-12-12 23:09:35 +0300

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num1 = '0';
+	int num2 = '0';
+
+	for (num1 = '0'; num1 <= '9'; num1++)
+	{
+		for (num2 = '0'; num2 <= '9'; num2++)
+		{
+			if (!((num1 == num2) || (num1 > num2)))
+			{
+				putchar(num1);
+				putchar(num2);
+				if (!(num2 == '9' && num1 == '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
