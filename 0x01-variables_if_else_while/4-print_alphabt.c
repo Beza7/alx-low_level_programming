b8a1af1dbe2879c7361add191d2797bbde3f3021
Beza7 <117745289+Beza7@users.noreply.github.com>
2022-12-12 23:01:36 +0300

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry poinnt
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e')
+			ch++;
+		else if (ch == 'q')
+			ch++;
+		else
+		{
+			putchar(ch);
+			ch++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
